3bdb1470a3d0fbfaabea326875ff08368de69cf0
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-01-20 17:47:14 +0100

    make the use case should return Fizz when 9 given pass

--- src/main/kotlin/main/FizzBuzz.kt
+++ src/main/kotlin/main/FizzBuzz.kt
@@ -6,7 +6,7 @@ class FizzBuzz {
             if (number == 5) {
                 return "Buzz"
             }
-            if (number == 3 || number == 6) {
+            if (number == 3 || number == 6 || number == 9) {
                 return "Fizz";
             }
             return number.toString();
