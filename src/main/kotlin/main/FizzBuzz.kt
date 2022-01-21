6959ae6cb83674680fcebb767e642f06f821f7da
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-01-21 12:39:07 +0100

    making the use case of should return FizzBuzz when a numbers it's divisible by 15 pass

--- src/main/kotlin/main/FizzBuzz.kt
+++ src/main/kotlin/main/FizzBuzz.kt
@@ -3,6 +3,9 @@ package main
 class FizzBuzz {
     companion object {
         fun convert(number: Int): String {
+            if (number == 15) {
+                return "FizzBuzz"
+            }
             if (isDivisibleByFive(number)) return "Buzz"
             if (isDivisibleByThree(number)) return "Fizz"
             return number.toString();
